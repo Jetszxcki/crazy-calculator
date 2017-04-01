297d350007b2235b2409d247cc211ae968fc7073
JethroAlbano <skyjeth1371@gmail.com>
2017-04-01 22:07:40 +0800

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -3,27 +3,48 @@ public class Queue {
 	
 	private PseudoArray arr;
 	private int rear = -1;
+	private int num;
 	
-	public Queue() {
-		arr = new PseudoArray(20);
+	public Queue(int num) {
+		
+		arr = new PseudoArray(10);
+		this.num = num;
+		
 	}
 	
 	public void enqueue(String item) {
+		
 		arr.add(item);
 		rear++;
+		Calculator.structures[num][rear].setBackground(Calculator.colors[rear]);
+		Calculator.structureItems[num][rear].setText(item);
+		
 	}
 	
 	public String dequeue() {
+		
+		int ctr = 1;
+		Calculator.structureItems[num][0].setText(" ");
+		
+		if(rear != 0) {
+			while(ctr < rear) {
+				Calculator.structureItems[num][ctr-1].setText(Calculator.structureItems[num][ctr].getText());
+				Calculator.structures[num][ctr-1].setBackground(Calculator.colors[ctr-1]);
+				ctr++;
+			}
+			Calculator.structureItems[num][rear].setText(" ");		
+			Calculator.structures[num][rear].setBackground(Calculator.colors[10]);
+		}
 		rear--;
+		if(rear == -1)
+			Calculator.structures[num][0].setBackground(Calculator.colors[10]);
+		
 		return arr.delete();
+		
 	}
 	
 	public int rr() {
 		return rear;
-	}
-	
-	public void setRear(int rear) {
-		this.rear = rear;
 	}
 	
 	public boolean isEmpty() {
