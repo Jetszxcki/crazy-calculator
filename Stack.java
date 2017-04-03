a5c2a0d6566730fcb99b52bbe7b94bde690b009e
JethroAlbano <skyjeth1371@gmail.com>
2017-04-03 21:46:52 +0800

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -3,6 +3,7 @@ public class Stack {
 	
 	private Queue q1;
 	private Queue q2;
+	private Thread thread;
 	public int elements = 0;
 	private boolean q1q2 = true;
 	
@@ -10,6 +11,12 @@ public class Stack {
 		
 		q1 = new Queue(1);
 		q2 = new Queue(2);
+		
+		thread = new Thread() { 
+			public void run() { 
+				pop(); 
+			} 
+		};
 		
 	}
 	
@@ -25,22 +32,36 @@ public class Stack {
 		
 	}
 	
+	
+	
 	public String pop() {
 		
 		String output = "";
+		thread = new Thread();
+		thread.start();
 		
-		if(q1q2) {
-			while(q1.rr() != 0)
-				q2.enqueue(q1.dequeue());
-			q1q2 = false;
-			output = q1.dequeue();
+		try{
 			
-		}else if(!q1q2){
-			while(q2.rr() != 0)
-				q1.enqueue(q2.dequeue());
-			q1q2 = true;
-			output = q2.dequeue();
-		}
+			if(q1q2) {
+				while(q1.rr() != 0) {
+					q2.enqueue(q1.dequeue());
+					Thread.sleep(500);
+				}
+				q1q2 = false;
+				output = q1.dequeue();
+				
+			}else if(!q1q2){
+				while(q2.rr() != 0) {
+					q1.enqueue(q2.dequeue());
+					Thread.sleep(500);
+				}
+				q1q2 = true;
+				output = q2.dequeue();
+			}
+			Thread.sleep(500);
+			
+		}catch(Exception e){ e.printStackTrace(); }
+		
 		elements--;
 		Calculator.structures[0][elements].setBackground(Calculator.colors[10]);
 		Calculator.structureItems[0][elements].setText(" ");
